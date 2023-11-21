5018207ae0a6ab225e24c3cff95ca532f5655cdf
Yusur <yusurmusse@hotmail.com>
2023-11-21 00:20:18 +0000

    Challenge: count_words - test-drive a single function, implement pass test

---
+++ lib/count_words.py
@@ -0,0 +1,6 @@
+# takes a string as an argument and returns 
+# the number of words in that string.
+
+def count_words(string):
+    sentence_length = len(string)
+    return sentence_length 
---
+++ tests/test_count_words.py
@@ -0,0 +1,15 @@
+# from lib.count_words import count_words
+
+# def test_a_string_and_returns_it():
+#     result = count_words("My name is Yusur")
+#     assert result == "My name is Yusur"
+
+# def test_a_longer_string_and_return_it():
+#     result = count_words("My name is Yusur and my birthday is 24 August.")
+#     assert result == "My name is Yusur and my birthday is 24 August."
+
+# def test_with_string_and_return_the_number_of_words_in_string():
+#     result = count_words("My name is Yusur and my birthday is 24 August.")
+#     assert result == 46
+
+    # Test drive single method program 
